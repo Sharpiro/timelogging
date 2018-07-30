146b0eefa742cbab2ce33241fe8d18a69d0671db
sharpiro <dsharpbb09@gmail.com>
2018-07-30 17:27:50 -0400

    starting on javascript storage client

---
+++ docs/links.md
@@ -0,0 +1,3 @@
+# Azure Table Storage
+- [emulator usage](https://docs.microsoft.com/en-us/azure/storage/common/storage-use-emulator)
+- [javascript client](https://azure.microsoft.com/en-us/blog/new-azure-storage-javascript-client-library-for-browsers-preview/)
--- src/app/reporting/reporting.component.ts
+++ src/app/reporting/reporting.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const TableStorage = (<any>window).AzureStorage.Table
+
 @Component({
   selector: 'app-reporting',
   templateUrl: './reporting.component.html',
@@ -10,6 +12,11 @@ export class ReportingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    console.log(TableStorage);
+    var tableSvc = TableStorage.createTableService("devstoreaccount1", "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==");
+    
+    // const blobService = azure.createBlobService()
   }
 
 }
